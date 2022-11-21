5594e52141f318e9f026b58429c44d97282aeb3a
douja <61593873+khadija20@users.noreply.github.com>
2022-11-21 16:17:37 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+
 const App = () => {
-  return <div>Hello world!</div>;
+//return <Cart/>;
+ //return <Home/>;
+  //return <Product/>;
+  //return <ProductList/>;
+ return <Register/>;
 };
 
 export default App;
